5a53456e0cc8e5423a8eaf514a5be27c7e9ef22f
Kevin <411190068@qq.com>
2016-09-02 11:04:18 +0800

    comment

--- src/main/java/com/kevin/pattern/singleton/Singleton.java
+++ src/main/java/com/kevin/pattern/singleton/Singleton.java
@@ -1,16 +1,17 @@
 package com.kevin.pattern.singleton;
 
 /**
+ * Get the instance when the variable is initialized
  * Created by Kevin on 2016/9/1.
  */
 public class Singleton {
 
-    private static final Singleton instance = new Singleton();
+    private static final Singleton INSTANCE = new Singleton();
 
     private Singleton(){}
 
     public static Singleton getInstance(){
-        return instance;
+        return INSTANCE;
     }
 
 
